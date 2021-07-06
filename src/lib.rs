11816b595b4a890b21913f5a873be0fcd72d6bca
wangjun861205 <444055828@qq.com>
2021-07-06 09:32:34 +0800

    remove rudundant wrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
 use futures::{stream::iter, SinkExt};
 use futures::{Stream, StreamExt};
-use std::pin::Pin;
 
 pub struct Spliter<S, I>
 where
     S: Stream<Item = I> + Send + Sync + Unpin,
     I: Clone,
 {
-    inner: Pin<Box<S>>,
+    inner: Box<S>,
     num: usize,
     txs: Vec<UnboundedSender<I>>,
     rxs: Vec<UnboundedReceiver<I>>,
@@ -19,7 +18,7 @@ where
     S: Stream<Item = I> + Send + Sync + Unpin + 'static,
     I: Clone + Send + Sync + 'static,
 {
-    pub fn new(stream: Pin<Box<S>>, num: usize) -> Self {
+    pub fn new(stream: Box<S>, num: usize) -> Self {
         Self {
             inner: stream,
             num: num,
@@ -50,7 +49,7 @@ where
 #[tokio::test]
 async fn test_split() {
     let stream = iter(vec![1, 2, 3, 4, 5, 6, 7]);
-    let spliter = Spliter::new(Box::pin(stream), 2);
+    let spliter = Spliter::new(Box::new(stream), 2);
     let rxs = spliter.split();
     for (i, mut rx) in rxs.into_iter().enumerate() {
         while let Some(v) = rx.next().await {
